18b5da6b39d84f5d0feae32831a3b74957db8206
alokkuri <alokkuri98@gmail.com>
2026-04-10 01:51:55 +0600

    TypeScript Lesson1, Lesson2

---
+++ Lesson02/build/js/main.js
@@ -0,0 +1,15 @@
+let myName = "Alok";
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'John';
+meaningOfLife = 42;
+isLoading = true;
+album = 'Van Halen';
+const sum = (a, b) => {
+    return a + b;
+};
+let postID;
+let isActive;
+let re = /\w+/g;
+export {};
---
+++ Lesson02/src/main.ts
@@ -0,0 +1,18 @@
+let myName: string = "Alok";
+let meaningOfLife: number ;
+let isLoading: boolean;
+let album: number | string;
+
+myName = 'John';
+meaningOfLife = 42;
+isLoading = true;
+album = 'Van Halen';
+
+const sum = (a : number, b : string)=>{
+    return a + b;
+}
+
+let postID : string | number
+let isActive : number | boolean
+
+let re : RegExp = /\w+/g
